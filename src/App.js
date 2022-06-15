067df3d778945b440da793fb0ccf8d4ccad4a6f1
Abdelrahman Elshershaby <abdelrahman@macs-MacBook-Pro.local>
2022-06-15 14:54:52 +0200

    adding footer section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import Questions from "./components/Questions";
 import Summary from "./components/Summary/Summary";
+import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 import "./styles/App.css";
 
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Summary></Summary>
       <Questions></Questions>
+      <Footer />
     </div>
   );
 }
---
+++ src/layout/Footer.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Col, FormControl, InputGroup, Row } from "react-bootstrap";
+import { Facebook, Google, Linkedin, Twitter } from "react-bootstrap-icons";
+import logo_dark from "../images/logo-dark.png";
+function Footer() {
+  return (
+    <div className="border-top mx-4 pt-3">
+      <Row>
+        <Col>
+          <div className="mb-3">
+            <img src={logo_dark} alt="logo" />
+          </div>
+          <div className="d-flex flex-column mt-2 gap-1">
+            <a href="mailto:safa@gmail.com" className="f-link">
+              safa@gmail.com
+            </a>
+            <a href="tel:+757 699-4478" className="tel f-link">
+              +757 699-4478
+            </a>
+          </div>
+        </Col>
+        <Col>
+          <p className="links">الروابط</p>
+          <Row>
+            <Col>
+              <p className="link"> الرئيسية</p>
+              <p className="link">من نحن</p>
+              <p className="link">الخدمات</p>
+              <p className="link">الفعاليات</p>
+              <p className="link">الكورسات</p>
+              <p className="link">الندوات</p>
+            </Col>
+            <Col>
+              <p className="link">الورشات</p>
+              <p className="link">الأخبار</p>
+              <p className="link">الأبحاث</p>
+              <p className="link">فريق العمل</p>
+              <p className="link"> العضوية</p>
+              <p className="link">اتصل بنا</p>
+            </Col>
+          </Row>
+        </Col>
+        <Col className="flex-column gap-3 d-flex">
+          <p className="links">تابعنا</p>
+          <div className="d-flex flex-column gap-1">
+            <div className="d-flex gap-1 f-icons">
+              <Google className="icon-size mt-0" />
+              <Linkedin className="icon-size mt-0" />
+              <Twitter className="icon-size mt-0" />
+              <Facebook className="icon-size mt-0" />
+            </div>
+            <p className="links">لمتابعه الاخبار</p>
+            <InputGroup className="mb-3">
+              <FormControl
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                className="rounded-1"
+              />
+              <InputGroup.Text
+                id="inputGroup-sizing-default"
+                className="px-3 bg-primary text-white rounded-1"
+              >
+                اشترك
+              </InputGroup.Text>
+            </InputGroup>
+          </div>
+        </Col>
+      </Row>
+      <div className="text-center py-5 link">الحقوق محفوظة لصفا</div>
+    </div>
+  );
+}
+
+export default Footer;
